cd71d700f32d8a69f0ea1f799d7872f8ea2529c5
ali.esmaeel@visticle.de <ali.esmaeel@visticle.de>
2026-06-02 21:58:07 +0100

    remove cache

--- storage/framework/views/ea4994ed00fd6436caadaf0675d5ba1c.php
+++
@@ -1,3 +0,0 @@
-<div class="cur" id="cur"></div>
-<div class="cur-ring" id="curRing"></div>
-<?php /**PATH /home/ali/Downloads/alarab/alarab-laravel-project/alarab-laravel/resources/views/components/site/cursor.blade.php ENDPATH**/ ?>
