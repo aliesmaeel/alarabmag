107823c5cc1eaf0d61373bb3d6d5c30cf12d10bb
Ali Esmaeel <ali.esma3iel@gmail.com>
2026-08-23 18:57:28 +0100

    change

--- config/ads.php
+++ config/ads.php
@@ -3,5 +3,5 @@
 return [
     'enabled' => env('ADSENSE_ENABLED', true),
     'client' => env('ADSENSE_CLIENT', 'ca-pub-6158011037590169'),
-    'slot' => env('ADSENSE_LEADERBOARD_SLOT', '5684308181'),
+    'slot' => env('ADSENSE_LEADERBOARD_SLOT', '5952026172'),
 ];
--- resources/views/components/site/adsense-leaderboard.blade.php
+++ resources/views/components/site/adsense-leaderboard.blade.php
@@ -1,1 +1,15 @@
-
+@if(config('ads.enabled'))
+<aside class="site-ad site-ad--leaderboard" aria-label="إعلان">
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6158011037590169"
+         crossorigin="anonymous"></script>
+    <ins class="adsbygoogle"
+         style="display:block"
+         data-ad-client="ca-pub-6158011037590169"
+         data-ad-slot="5952026172"
+         data-ad-format="auto"
+         data-full-width-responsive="true"></ins>
+    <script>
+         (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+</aside>
+@endif
